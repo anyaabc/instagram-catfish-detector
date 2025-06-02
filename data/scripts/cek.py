78638571fdbbe06b7646b04fec2ae37a6900606d
anyaabc <126607618+anyaabc@users.noreply.github.com>
2025-06-02 21:35:24 +0700

    update

--- data/scripts/cek.py
+++ data/scripts/cek.py
@@ -1,37 +1,5 @@
 # # file: data/scripts/cek.py
 
-# import sys
-# import os
-
-# # Tambahkan path ke folder src/
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
-
-# import mysql.connector
-# from backend.config import DB_CONFIG
-
-# # Cek isi tabel
-# conn = mysql.connector.connect(**DB_CONFIG)
-# cursor = conn.cursor()
-
-# cursor.execute("SELECT COUNT(*) FROM instagram_users;")
-# users_count = cursor.fetchone()[0]
-# print(f"👤 Users: {users_count}")
-
-# cursor.execute("SELECT COUNT(*) FROM instagram_posts;")
-# posts_count = cursor.fetchone()[0]
-# print(f"📷 Posts: {posts_count}")
-
-# cursor.execute("SELECT COUNT(*) FROM face_embeddings;")
-# embeddings_count = cursor.fetchone()[0]
-# print(f"🧠 Embeddings: {embeddings_count}")
-
-# cursor.close()
-# conn.close()
-
-
-
-# cek database
-
 import sys
 import os
 
@@ -41,25 +9,25 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../s
 import mysql.connector
 from backend.config import DB_CONFIG
 
+# Cek isi tabel
+conn = mysql.connector.connect(**DB_CONFIG)
+cursor = conn.cursor()
 
-def cek_tabel():
-    conn = mysql.connector.connect(**DB_CONFIG)
-    cursor = conn.cursor()
+cursor.execute("SELECT COUNT(*) FROM instagram_users;")
+users_count = cursor.fetchone()[0]
+print(f"👤 Users: {users_count}")
 
-    cursor.execute("SHOW TABLES")
-    tables = cursor.fetchall()
-    print("Tables in DB:")
-    for t in tables:
-        print(t[0])
+cursor.execute("SELECT COUNT(*) FROM instagram_posts;")
+posts_count = cursor.fetchone()[0]
+print(f"📷 Posts: {posts_count}")
 
-    cursor.execute("DESCRIBE instagram_users")
-    print("\nStructure of instagram_users:")
-    for row in cursor.fetchall():
-        print(row)
+cursor.execute("SELECT COUNT(*) FROM face_embeddings;")
+embeddings_count = cursor.fetchone()[0]
+print(f"🧠 Embeddings: {embeddings_count}")
 
-    cursor.close()
-    conn.close()
+cursor.close()
+conn.close()
 
-if __name__ == "__main__":
-    cek_tabel()
 
+
+#/
